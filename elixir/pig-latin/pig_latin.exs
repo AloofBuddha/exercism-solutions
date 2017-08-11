e4dece87bcdfdc1bb64e37563f8e3d6dc56b4bcb
Benjamin Cohen <bac1087@gmail.com>
2017-08-11 13:07:22 -0400

    pig latin

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -13,8 +13,33 @@ defmodule PigLatin do
 
   Some groups are treated like vowels, including "yt" and "xr".
   """
+
+  @regex_vowel ~r/^([aeiou]|yt|xr)/
+  @regex_consonant ~r/^(sch|squ|thr|ch|qu|th|[bcdfghjklmnpqrstvwxyz])/
+
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase 
+    |> String.split(" ")
+    |> Enum.map(&translate_word/1)
+    |> Enum.join(" ")
+  end
+
+  # translates an individual word to pig latin
+  defp translate_word(phrase) do
+    cond do
+      Regex.match? @regex_vowel, phrase -> 
+        phrase <> "ay"
+
+      Regex.match? @regex_consonant, phrase ->
+        {cons, rest} = split_on_consonant phrase
+        rest <> cons <> "ay"
+    end
+  end
+
+  defp split_on_consonant(phrase) do
+    Regex.split(@regex_consonant, phrase, [include_captures: true, trim: true])
+    |> List.to_tuple
   end
 end
 
